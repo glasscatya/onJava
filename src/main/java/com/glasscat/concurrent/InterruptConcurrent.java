28c57906d89402cbd89729d60e0f3aeaec875ce7
yufang <2721381743@qq.com>
2024-10-13 21:12:44 +0800

    10/13

---
+++ src/main/java/com/glasscat/concurrent/InterruptConcurrent.java
@@ -0,0 +1,42 @@
+package com.glasscat.concurrent;
+
+// 中断线程
+public class InterruptConcurrent {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new MyThread();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt(); // 中断t线程
+        t.join(); // 等待t线程结束
+        System.out.println("end");
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        Thread hello = new HelloThread();
+        hello.start(); // 启动hello线程
+        try {
+            hello.join(); // 等待hello线程结束
+        } catch (InterruptedException e) {
+            System.out.println("interrupted!");
+        }
+        System.out.println("我中断了");
+        hello.interrupt();
+    }
+}
+
+class HelloThread extends Thread {
+    public void run() {
+        int n = 0;
+        while (!isInterrupted()) {
+            n++;
+            System.out.println(n + " hello!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
